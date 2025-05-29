9bdd4bdb00b7ffb2876d9d922f805a331eb7a840
Jason Macias <jason.macias2499@gmail.com>
2025-05-28 19:41:37 -0500

    Add files via upload

---
+++ social/cmd/api/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"github.com/jsnm09/social/internal/env"
+	"github.com/jsnm09/social/internal/store"
+)
+
+func main() {
+	cfg := config{
+		addr: env.GetString("ADDR", ":8080"),
+	}
+
+	store := store.NewStorage(nil)
+
+	app := &application{
+		config: cfg,
+		store:  store,
+	}
+
+	mux := app.mount()
+	log.Fatal(app.run(mux))
+}
