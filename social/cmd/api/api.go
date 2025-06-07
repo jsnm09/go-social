8c7e5be45456097f94138904735188542bcec9f4
jsnm09 <jason.macias2499@gmail.com>
2025-06-07 00:42:34 -0500

    created a template for database and created a docker file

--- social/cmd/api/api.go
+++ social/cmd/api/api.go
@@ -17,6 +17,14 @@ type application struct {
 
 type config struct {
 	addr string
+	DB   dbConfig
+}
+
+type dbConfig struct {
+	addr         string
+	maxOpenConns int
+	maxIdleConns int
+	maxIdleTime  string
 }
 
 func (app *application) mount() http.Handler {
--- social/cmd/api/main.go
+++ social/cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/jsnm09/social/internal/db"
 	"github.com/jsnm09/social/internal/env"
 	"github.com/jsnm09/social/internal/store"
 )
@@ -10,9 +11,28 @@ import (
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		DB: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.DB.addr,
+		cfg.DB.maxOpenConns,
+		cfg.DB.maxIdleConns,
+		cfg.DB.maxIdleTime,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Println("database connection pool established")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
